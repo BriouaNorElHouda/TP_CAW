d482dcedff2aee55d241bda15a9fa8b89501ae6f
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-11-05 22:34:06 +0100

    Create grep.js

---
+++ TP5/grep.js
@@ -0,0 +1,20 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+async function checkIfContainsAsync(exp, file) {
+  try {
+    const contents = await fsPromises.readFile(file, 'utf-8');
+
+    const res = contents.includes(exp);
+    if(res==true){
+      console.log(file);
+    }else{
+      console.log("NONE");
+    }
+    
+
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+checkIfContainsAsync('logger' ,'./exo4.js');
