47c331bb7e94272ce2f6e52ab3cfbf5a700dc4e5
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-11-06 22:00:44 +0100

    Update grep.js

--- TP5/grep.js
+++ TP5/grep.js
@@ -1,21 +1,88 @@
-const {readFileSync, promises: fsPromises} = require('fs');
-async function grep(exp,file) {
+const { readFileSync, readdirSync, lstatSync } = require("fs");
 
-  try {
-    const contents = await fsPromises.readFile(file, 'utf-8');
-
-    const res = contents.includes(exp);
-    if(res==true){
-      console.log('found in file ',file);
-    }else{
-      console.log("NONE");
+function searching(path, exp) {
+    try {
+        if (path.toLowerCase().endsWith("")) {
+            let file = readFileSync(path, "utf-8");
+            if (file.search(exp) != -1) return true
+        }
+        return false
+    } catch (err) {
+        return false
     }
-    
-
-    return res;
-  } catch (err) {
-    console.log(err);
-  }
 }
 
-grep('logger' ,'./exo4.js');
+
+
+
+function dirsearch(path, exp) {
+    try {
+        let dirfiles = readdirSync(path);
+        let res = [] ;
+        dirfiles.forEach((file) => {
+            let state = lstatSync(path + "/" + file);
+            if (state.isDirectory()) {
+                let dirRes = dirsearch(path + "/" + file, exp);
+                dirRes.length != 0 ? res.push(...dirRes) : null
+            } else if (file.toLowerCase().endsWith("")) {
+                searching(path + "/" + file, exp) ? res.push(path + "/" + file) : null
+            }
+        })
+        return res
+    } catch (err) {
+        return []
+    }
+}
+
+
+
+
+function Searchfile(exp, ...args) {
+   let expr = new RegExp(exp, "i");
+   let res = [];
+  args.forEach((path) => {
+       searching(path, expr) ? res.push(path) : null
+   });
+   if (res.length == 0) return "NONE"
+   console.log('found in file: ')
+    return res;
+   
+}
+
+
+
+
+function Searchdir(exp, ...args) {
+    let expr = new RegExp(exp, "i");
+    let res = [];
+    args.forEach((path) => {
+        let state = lstatSync(path);
+        if (path.toLowerCase().endsWith("")) {
+            searching(path, expr) ? res.push(path) : null
+        } else if (state.isDirectory()) {
+            let dirRes = dirsearch(path, expr);
+            dirRes.length != 0 ? res.push(...dirRes) : null
+        }
+
+    });
+
+    if (res.length == 0) return "NONE"
+    console.log('found in directory: ')
+    return  res;
+}
+
+
+
+
+// EX 3
+
+// QUESTION A
+
+console.log(Searchfile("Hope World", "./Kounouz.txt", "./Nour.js", "./Nour.txt"));
+
+// QUESTION B
+
+console.log(Searchdir("search", "./Kounouz.txt", "./Nour.txt",  "./informatique"));
+
+
+//by Nor el houda BRIOUA  & Kounouz HEDJILA
